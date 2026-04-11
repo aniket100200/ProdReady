00848ff858796d76c8963e0f6842c23ddcc764c5
Aniket Khangar <aniketkhangar459@gmail.com>
2026-04-11 16:31:38 +0530

    removed Dynamic Html

--- src/main/java/ProdReady/Aniket/spring/DynamicHtml/Thymleaf/HomeController.java
+++ src/main/java/ProdReady/Aniket/spring/DynamicHtml/Thymleaf/HomeController.java
@@ -1,14 +1,23 @@
 package ProdReady.Aniket.spring.DynamicHtml.Thymleaf;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+//@Controller
+//public class HomeController {
+//  @GetMapping("/home")
+//  public String home(Model model) {
+//    model.addAttribute("message", "Welcome to Springboot Thymeleaf");
+//    return "home";
+//  }
+//}
 
 @Controller
 public class HomeController {
-  @GetMapping("/home")
-  public String home(Model model) {
-    model.addAttribute("message", "Welcome to Springboot Thymeleaf");
-    return "home";
-  }
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
 }
+
+
